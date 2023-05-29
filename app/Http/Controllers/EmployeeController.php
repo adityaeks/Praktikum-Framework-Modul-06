3fc66544ec2642a0904dfa2992ccc6e32ffd790a
adityaeks <mohadityaekosaputra11@gmail.com>
2023-05-29 18:25:41 +0700

    first commit

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use App\Models\Employee;
+use App\Models\Position;
 // use App\Http\Controllers\DB;
 
 
@@ -19,80 +21,78 @@ class EmployeeController extends Controller
         $pageTitle = 'Employee List';
 
         // RAW SQL QUERY
-        $employees = DB::select('
-            select *, employees.id as employee_id
-            from employees
-            left join positions on employees.position_id = positions.id
-        ');
+        // $employees = DB::select('
+        //     select *, employees.id as employee_id
+        //     from employees
+        //     left join positions on employees.position_id = positions.id
+        // ');
+
+        // return view('employee.index', [
+        //     'pageTitle' => $pageTitle,
+        //     'employees' => $employees
+        // ]);
+        // ELOQUENT
+        $employees = Employee::all();
 
         return view('employee.index', [
             'pageTitle' => $pageTitle,
             'employees' => $employees
         ]);
-    }
+        }
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
-    {
-        $pageTitle = 'Create Employee';
-        // RAW SQL Query
-        $positions = DB::select('select * from positions');
-
-        return view('employee.create', compact('pageTitle', 'positions'));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
 {
-    $messages = [
-        'required' => ':Attribute harus diisi.',
-        'email' => 'Isi :attribute dengan format yang benar',
-        'numeric' => 'Isi :attribute dengan angka'
-    ];
+    $pageTitle = 'Create Employee';
 
-    $validator = Validator::make($request->all(), [
-        'firstName' => 'required',
-        'lastName' => 'required',
-        'email' => 'required|email',
-        'age' => 'required|numeric',
-    ], $messages);
+    // ELOQUENT
+    $positions = Position::all();
 
-    if ($validator->fails()) {
-        return redirect()->back()->withErrors($validator)->withInput();
-    }
-
-    // INSERT QUERY
-    DB::table('employees')->insert([
-        'firstname' => $request->firstName,
-        'lastname' => $request->lastName,
-        'email' => $request->email,
-        'age' => $request->age,
-        'position_id' => $request->position,
-    ]);
-
-    return redirect()->route('employees.index');
+    return view('employee.create', compact('pageTitle', 'positions'));
 }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function store(Request $request)
+    {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // ELOQUENT
+        $employee = New Employee;
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+        return redirect()->route('employees.index');
+    }
+
+
+        public function show(string $id)
     {
         $pageTitle = 'Employee Detail';
 
-    // RAW SQL QUERY Done ke queery builder
-        $employee = DB::table('employees')
-            ->select('employees.*', 'positions.name as position_name')
-            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
-            ->where('employees.id', $id)
-            ->first();
+        // ELOQUENT
+        $employee = Employee::find($id);
 
         return view('employee.show', compact('pageTitle', 'employee'));
-
     }
 
     /**
@@ -102,24 +102,14 @@ class EmployeeController extends Controller
 {
     $pageTitle = 'Edit Employee';
 
-    $positions = DB::table('positions')->get();
-    $employee = DB::table('employees')
-        ->select('*', 'employees.id as employee_id', 'positions.id as position_id')
-        ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
-        ->where('employees.id', $id)
-        ->first();
+    // ELOQUENT
+    $positions = Position::all();
+    $employee = Employee::find($id);
 
-    return view('employee.edit', compact('pageTitle', 'employee', 'positions'));
+    return view('employee.edit', compact('pageTitle', 'positions', 'employee'));
 }
 
-
-
-
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+public function update(Request $request, string $id)
 {
     $messages = [
         'required' => ':Attribute harus diisi.',
@@ -138,18 +128,16 @@ class EmployeeController extends Controller
         return redirect()->back()->withErrors($validator)->withInput();
     }
 
-    DB::table('employees')
-    ->where('id', $id)
-    ->update([
-        'firstname' => $request->firstName,
-        'lastname' => $request->lastName,
-        'email' => $request->email,
-        'age' => $request->age,
-        'position_id' => $request->position,
-    ]);
+    // ELOQUENT
+    $employee = Employee::find($id);
+    $employee->firstname = $request->firstName;
+    $employee->lastname = $request->lastName;
+    $employee->email = $request->email;
+    $employee->age = $request->age;
+    $employee->position_id = $request->position;
+    $employee->save();
 
     return redirect()->route('employees.index');
-
 }
 
 
@@ -157,15 +145,13 @@ class EmployeeController extends Controller
      * Remove the specified resource from storage.
      */
 
-    public function destroy(string $id)
-    {
-        // QUERY BUILDER
-        DB::table('employees')
-            ->where('id', $id)
-            ->delete();
+     public function destroy(string $id)
+     {
+         // ELOQUENT
+         Employee::find($id)->delete();
 
-        return redirect()->route('employees.index');
-    }
+         return redirect()->route('employees.index');
+     }
 
 
 }
